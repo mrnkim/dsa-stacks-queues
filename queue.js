1b06db97e6ae38141f98c03fb58c253c53f3a5de
MKim <mrn.kim07@gmail.com>
2023-06-12 15:43:14 -0700

    finish enque() and deque()

---
+++ queue.js
@@ -0,0 +1,59 @@
+/** Node: node for a queue. */
+
+class Node {
+  val = null;
+  next = null;
+
+  constructor(val) {
+    this.val = val;
+  }
+}
+
+/** Queue: chained-together nodes where you can
+ *  remove from the front or add to the back. */
+
+class Queue {
+  first = null;
+  last = null;
+  size = 0;
+
+  /** enqueue(val): add new value to end of the queue. Returns undefined. */
+  enqueue(val) {
+    const newNode = new Node(val);
+    if (this.size === 0) {
+      this.first = newNode;
+    } else {
+      this.last.next = newNode;
+    }
+    this.last = newNode;
+    this.size++;
+  }
+
+  /** dequeue(): remove the node from the start of the queue
+   * and return its value. Should throw an error if the queue is empty. */
+  // a   b   c
+  dequeue() {
+    if (this.size === 0) {
+      throw new Error();
+    }
+    let atFront = this.first;
+    if (this.size === 1) {
+      this.first = null;
+      this.last = null;
+    } else {
+      this.first = this.first.next;
+    }
+    this.size--;
+    return atFront.val;
+  }
+
+  /** peek(): return the value of the first node in the queue. */
+
+  peek() {}
+
+  /** isEmpty(): return true if the queue is empty, otherwise false */
+
+  isEmpty() {}
+}
+
+module.exports = Queue;
