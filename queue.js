3a743e68b2286ae69d7b257ef308d805caac8290
Timothy Sukamtoh <timtoh168@gmail.com>
2023-06-12 17:05:20 -0700

    fixed queue linked list

--- queue.js
+++ queue.js
@@ -62,3 +62,4 @@ class Queue {
 }
 
 module.exports = Queue;
+
--- queue.linkedLists.js
+++ queue.linkedLists.js
@@ -1,5 +1,4 @@
-import LinkedList from "./linkedList";
-
+const LinkedList = require("./linkedList");
 /** Node: node for a queue. */
 
 class Node {
@@ -23,12 +22,19 @@ class Queue {
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
   enqueue(val) {
     this._list.push(val);
+    this.first = this._list.head;
+    this.last = this._list.tail;
+    this.size = this._list.length;
   }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
   dequeue() {
-    return this._list.shift();
+    const value = this._list.shift();
+    this.first = this._list.head;
+    this.last = this._list.tail;
+    this.size = this._list.length;
+    return value;
   }
 
   /** peek(): return the value of the first node in the queue. */
